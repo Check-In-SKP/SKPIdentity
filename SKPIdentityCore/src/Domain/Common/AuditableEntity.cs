1c41a7285ba21b7b0a844d2eae24dd6118eadb22
SnitzelKlaus <SnitzelKlaus@gmail.com>
2023-11-30 23:05:02 +0100

    Minor update

---
+++ SKPIdentityCore/src/Domain/Common/AuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public class AuditableEntity : DomainEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTimeOffset LastModifiedAt { get; set; }
+    }
+}
