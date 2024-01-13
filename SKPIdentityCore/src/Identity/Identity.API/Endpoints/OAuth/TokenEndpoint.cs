3b9bcf2a6e433f52ccabf1ee3176631d9714346e
SnitzelKlaus <SnitzelKlaus@gmail.com>
2024-01-13 01:36:52 +0100

    OAuth test

--- SKPIdentityCore/src/Identity/Identity.API/Endpoints/OAuth/TokenEndpoint.cs
+++ SKPIdentityCore/src/Identity/Identity.API/Endpoints/OAuth/TokenEndpoint.cs
@@ -1,10 +1,15 @@
-﻿using System.Text;
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 
 namespace Identity.API.Endpoints.OAuth
 {
     public static class TokenEndpoint
     {
-        public static async Task<IResult> Handle(HttpRequest request)
+        public static async Task<IResult> Handle(HttpRequest request, DevKeys devKeys, IDataProtectionProvider dataProtectionProvider)
         {
             var bodyBytes = await request.BodyReader.ReadAsync();
             var bodyContent = Encoding.UTF8.GetString(bodyBytes.Buffer);
@@ -21,7 +26,38 @@ namespace Identity.API.Endpoints.OAuth
                 else if (key == "code_verifier") codeVerifier = value;
             }
 
-            return Results.Ok();
+            var protector = dataProtectionProvider.CreateProtector("oauth");
+            var codeString = protector.Unprotect(code);
+            var authCode = JsonSerializer.Deserialize<AuthCode>(codeString);
+
+            if(!ValidateCodeVerifier(authCode, codeString))
+            {
+                return Results.BadRequest();
+            }
+
+            var handler = new JsonWebTokenHandler();
+            return Results.Ok(new
+            {
+                access_token = handler.CreateToken(new SecurityTokenDescriptor()
+                {
+                    Claims = new Dictionary<string, object>()
+                    {
+                        [JwtRegisteredClaimNames.Sub] = Guid.NewGuid().ToString(),
+                        ["custom"] = "foo"
+                    },
+                    Expires = DateTime.Now.AddMinutes(15),
+                    TokenType = "Bearer",
+                    SigningCredentials = new SigningCredentials(devKeys.SecurityKey, SecurityAlgorithms.RsaSha256)
+                }),
+                token_type = "Bearer"
+            });
+        }
+
+        private static bool ValidateCodeVerifier(AuthCode code, string codeVerifier)
+        {
+            var sha256 = SHA256.Create();
+            var codeChallenge = Base64UrlEncoder.Encode(sha256.ComputeHash(Encoding.ASCII.GetBytes(codeVerifier)));
+            return code.CodeChallenge == codeChallenge;
         }
     }
 }
